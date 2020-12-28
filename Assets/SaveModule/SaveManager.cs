7194b6ccae59cd11e7b4e42f30a1b3c14e55f1a4
kindezen <kindezen@naver.com>
2020-12-28 21:18:50 +0900

    sdf

---
+++ Assets/SaveModule/SaveManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using LitJson;
+
+public class SaveManager : MonoBehaviour
+{
+    /** 데이터 저장용 클래스 **/
+    /*
+     
+    System.IO의 Path.Combine(A, B)                         경로를 합칠 수 있다.
+    JsonUtility.ToJson(데이터, true)                       객체를 string으로 변환한다.
+    JsonUtility.FromJson<객체타입>(string화된 데이터)       객체를 뽑아낸다.
+    File.WriteAllText(경로, 문자열)                        경로에 있는 파일에 문자열을 덮어쓴다.
+    File.ReadAllText(경로)                                 경로에 있는 파일의 문자열을 뽑아낸다.
+     
+    */
+
+    public PlayerData[] playerData;
+
+    [ContextMenu("Save Json Data")]
+    void SavePlayerDataToJson()
+    {
+        string jsonData = JsonMapper.ToJson(playerData); //Par1: 변환할 객체, Par2: 이뿌게 정렬 여부
+        string path = Path.Combine(Application.dataPath, "playerData.json");
+        File.WriteAllText(path, jsonData);
+    }
+
+    [ContextMenu("Read Json Data")]
+    void LoadPlayerDataToJson()
+    {
+        
+        string path = Path.Combine(Application.dataPath, "playerData.json");
+        string jsonData = File.ReadAllText(path);
+        playerData = JsonMapper.ToObject<PlayerData[]>(jsonData);
+    }
+}
+
+[System.Serializable]
+public class PlayerData
+{
+    public string name;
+    public int age;
+    public int level;
+    public bool isDead;
+    public string[] items;
+}
---
+++ Assets/SaveModule/SaveModule.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+public class SaveModule : MonoBehaviour
+{
+    /** 저장 모듈 **/
+    /*
+    
+    <기능>
+        - 로컬 저장 (저장할 데이터) 리스트 데이터, 배열 데이터, 기본 데이터 저장 메소드 따로 구현
+        - 로컬 데이터 로드 : json으로 묶어서 한꺼번에 로드?
+
+    */
+
+    public string dataPath { get { return Application.persistentDataPath + "/Resources/SaveData"; } }
+
+    public void Save<T>(string _key, T _data)
+    {
+        string sdata = JsonUtility.ToJson(_data);
+        string path = Path.Combine(dataPath, _key + ".json");
+        File.WriteAllText(path, sdata);
+    }
+
+    public T Load<T>(string _key)
+    {
+        string sdata = Resources.Load<TextAsset>("SaveData/" + _key).text;
+        return JsonUtility.FromJson<T>(sdata);
+    }
+}
